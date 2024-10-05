724117292e96825a74fb5da64b51fa900a2a217a
Margarette Lipardo <136333284+teragrohm@users.noreply.github.com>
2024-10-06 05:07:20 +0800

    Add files via upload

--- Main Controller Codes/Main Control Unit/Arduino-UNO/Arduino-UNO.ino
+++ Main Controller Codes/Main Control Unit/Arduino-UNO/Arduino-UNO.ino
@@ -2,69 +2,100 @@
 #include <Servo.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#include <EEPROM.h>
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
-SoftwareSerial ArduinoUno(3,2);
+SoftwareSerial gsmModule(3,2);
 Servo feeder;
+Servo dispenser;
 
-int valid_inserted = 0;
+int buzzer_pin = 8;
+int proximity = 10;
+
+int multiplier[3] = {1, 0, 1};
+
+int previous_millis = 0;
+//int valid_inserted = 7;
+uint8_t valid_inserted = 0;
 int max_bottles = 10;
 int ballpen_count = 10;
 bool reward;
+bool full_of_bottles = false;
+
+String data;
+String displayIP = "To enter new WiFi details go to http://192.168.10.80:100 To start image processing go to http://";
+String defaultIP = "192.168.10.80:100";
+String cameraIP;
 
 //bool val;
 
-int feederActuator() {
+int feederActuator(int num) {
 
-  int multiplier[3] = {0, 1, 0};
+  //int multiplier[3] = {1, 1, 1};
   int i;
   int servo_position;
 
+//  result = num;
+
+  /* if (num == 0) 
+  {
+    multiplier[1] -= 1;
+  } */
+  if (num == 1) 
+  {
+    multiplier[1] += 2;
+  }
+
+  Serial.println(num);
+
   for (i = 0; i < 3; i++) {
 
-    servo_position = multiplier[i] * 180;
+    servo_position = multiplier[i] * 90;
     Serial.print("Servo will now turn ");
     Serial.print(servo_position);
     Serial.println(" degrees.");
     feeder.write(servo_position);
-    delay(3000 + (3000 * multiplier[i]));
+    //delay(3000 + (3000 * multiplier[i]));
+    delay(3000);
   }
+  multiplier[1] = 0;
   return servo_position;
 }
 
 void resetBottleCount() {
 
-  lcd.setCursor(14,0);
+  lcd.setCursor(0,0);
   lcd.print("0");
-  lcd.setCursor(15,0);
+  lcd.setCursor(1,0);
   lcd.print(valid_inserted);
-  lcd.setCursor(10,1);
+  lcd.setCursor(3,0);
   lcd.print("out of 10");
-  lcd.setCursor(9,2);
-  lcd.print("BOTTLES for");
-  lcd.setCursor(10,3);
-  lcd.print("a  reward");
+  lcd.setCursor(13,0);
+  lcd.print("BOTTLES");
+  lcd.setCursor(3,1);
+  lcd.print("to get a reward");
 }
 
-void updateBottleCount() {
+void updateBottleCount(int num) {
 
-  if (feederActuator() == 0) {
+  if (feederActuator(num) == 90) {
 
     if (valid_inserted < max_bottles) {
 
-      valid_inserted++;
+      valid_inserted += 1;
       Serial.print("Bottles inserted: ");
       Serial.println(valid_inserted);
 
-      lcd.setCursor(15,0);
+      lcd.setCursor(1,0);
     }
     if (valid_inserted == max_bottles) {
-      lcd.setCursor(14,0);
+      lcd.setCursor(0,0);
       reward = true;
    }
     // Write number of bottles to LCD Display
     lcd.print(valid_inserted);   
   }
+  EEPROM.write(10, valid_inserted);
 }
 
 void dispenseReward() {
@@ -75,12 +106,20 @@ void dispenseReward() {
   lcd.print("DISPENSING 1 BALLPEN");
   lcd.setCursor(0,2);
   lcd.print("Collect your reward!");
-  ballpen_count--;
 
+  dispenser.write(90);
+  delay(500);
+  dispenser.write(0);
+  delay(500);
+
+  ballpen_count -= 1;
+  //EEPROM.write(5, ballpen_count);
+/*
   if (ballpen_count == 0) {
     resetBallpenCount();
   }
-
+*/
+  raiseAlert();
   delay(5000);
 
   valid_inserted = 0;
@@ -89,16 +128,52 @@ void dispenseReward() {
   reward = false;
 }
 
-void resetBallpenCount() {
+void raiseAlert() {
 
-  Serial.println("An alert for empty ballpen bin will now be sent via SMS");
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("THE BALLPEN BIN");
-  lcd.setCursor(0,1);
-  lcd.print("IS EMPTY!");
-  lcd.setCursor(0,2);
-  lcd.print("Please refill it.");
+  gsmModule.println("AT+CMGF=1"); // Configuring TEXT mode
+  updateSerial();
+  gsmModule.println("AT+CMGS=\"+639xxxxxxxxx\""); //change ZZ with country code and xxxxxxxxxxx with phone number to sms
+  delay(500);
+
+  if (ballpen_count == 0) {
+
+    gsmModule.print("Reverse vendo ballpen bin is empty! Please refill it."); // SMS alert
+    updateSerial();
+    gsmModule.write(26);
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("THE BALLPEN BIN");
+    lcd.setCursor(0,1);
+    lcd.print("IS EMPTY!");
+    lcd.setCursor(0,2);
+    lcd.print("Please refill it.");
+  }
+  if (full_of_bottles) {
+
+    gsmModule.print("Reverse vendo bottle bin is full! Please collect inserted bottles."); // SMS alert
+    updateSerial();
+    gsmModule.write(26);
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("THE BOTTLE BIN");
+    lcd.setCursor(0,1);
+    lcd.print("IS FULL!");
+    lcd.setCursor(0,2);
+    lcd.print("Please empty it.");
+  }
+
+if (ballpen_count == 0 || full_of_bottles) {
+  for (int j = 0; j < 5; j++) {
+    for (int i = 0; i < 3; i++) {
+
+      bool pin_stat = multiplier[i] == 1;
+      digitalWrite(buzzer_pin, pin_stat);
+      delay(500);
+    }
+  } 
+ }
 }
 
 void invalidWarning() {
@@ -113,44 +188,141 @@ void invalidWarning() {
   resetBottleCount();
 }
 
+void scrollText(int row, String message, int interval, int col) {
+
+  // for (int i = 0; i < col; i++) {
+  //  message = " " + message;  
+  //} 
+  //message = message + " "; 
+  for (int position = 0; position < (message.length() + 1 - col); position++) {
+
+    if (Serial.available() > 0) break;
+
+    lcd.setCursor(0, row);
+    lcd.print(message.substring(position, position + col));
+    delay(interval);
+
+  }
+}
+
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available()) 
+  {
+    gsmModule.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(gsmModule.available()) 
+  {
+    Serial.write(gsmModule.read());//Forward what Software Serial received to Serial Port
+  }
+}
+
 void setup(){
 	
 	Serial.begin(9600);
-	ArduinoUno.begin(4800);
+	gsmModule.begin(9600);
+  valid_inserted = EEPROM.read(10);
+  //ballpen_count = EEPROM.read(5);
+  delay(1000);
 
   feeder.attach(7);
-  feeder.write(0);
+  feeder.write(90);
+
+  dispenser.attach(9);
+  dispenser.write(0);
+
+  pinMode(buzzer_pin, OUTPUT);
+  digitalWrite(buzzer_pin, LOW);
+
+  pinMode(proximity, INPUT);
 
   lcd.init();					
   lcd.backlight();
   resetBottleCount();
+
+  gsmModule.println("AT"); 
+  updateSerial();
 }
 
 void loop(){
+
+  scrollText(3, displayIP + cameraIP, 250, 20);
+  if (!(Serial.available() > 0)) delay(5000);
+  
 	
+String currentIP;
+
 	//while(ArduinoUno.available()>0){
   while(Serial.available()>0){
-	//float val = ArduinoUno.parseFloat();
   //int val = ArduinoUno.parseInt();
-  int val = Serial.parseInt();
-  //String val = ArduinoUno.readString();
-	//if(ArduinoUno.read()== '\n'){
-  if(Serial.read()== '\n'){
-    Serial.println(val);
-    if (val == 0) {
-      Serial.println("Invalid");
-      feederActuator();
+  //if(ArduinoUno.read()== '\n'){
+  int result;
+
+  char received = Serial.read();
+  //data += received;
+  
+  if (received == '\n') {
+
+    //data[data.length()] = 0;
+    //Serial.println(cameraIP);
+
+    Serial.println(data);
+    Serial.println(currentIP);
+
+    if (data == "Invalid") 
+    {
+      result = 0;
+      feederActuator(result);
       invalidWarning();
     }
-    if (val == 1) {
+
+    else if (data == "Valid") 
+    {
+      result = 1;
+      updateBottleCount(result);
+    }
+
+    cameraIP = currentIP;
+    data = "";
+    currentIP = "";
+    //cameraIP = "";    
+  }
+  else if (data.indexOf("CameraIP:") >= 0) 
+  {
+    currentIP += received;
+  }
+  else
+  {
+    data += received;
+  }
+
+  
+/*
+  if(Serial.read()== '\n'){
+    Serial.println(val);
+   if (val == 0) {
+    //if (val.indexOf("Invalid") >= 0) {
+      Serial.println("Invalid");
+      //feederActuator(val);
+      //invalidWarning();
+    }
+   /*if (val == 1) {
       // feeder.write(180);
       Serial.println("Valid");
-      updateBottleCount();      
-    }
-	 }
+      updateBottleCount(val);      
+    }*/
+	 //} 
    if (reward) {
     dispenseReward();
    }
- }  
+
+   while (digitalRead(proximity) == HIGH && (millis() - previous_millis >= 10000))
+   {
+    if (digitalRead(proximity) == LOW) break;
+   }
+   if (digitalRead(proximity) == HIGH) full_of_bottles = true;
+ }
+
 //delay(30);
 }
